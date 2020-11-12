b8c3b8292c42cf196fe9f46d3e1ce957ba939ff6
David Ruiz <davidl-ruizc@outlook.com>
2020-11-12 16:12:32 -0500

    airline, coc, go, tmux files

--- bin/nvim-setup.sh
+++ bin/nvim-setup.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.vimrc -O $HOME/.vimrc
+wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.go.vimrc -O $HOME/.go.vimrc
+wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.coc.vimrc -O $HOME/.coc.vimrc
+wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.airline.vimrc -O $HOME/.airline.vimrc
+wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.tmux.conf -O $HOME/.tmux.conf
+wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.zprofile -O $HOME/.zprofile
 
 echo "neovim setup done <3"
