d21f670dd75ff82f33df81981eca4f83d7c4e13b
David Ruiz <davidl-ruizc@outlook.com>
2021-05-30 11:02:16 -0500

    update setup

--- bin/nvim-setup.sh
+++ bin/nvim-setup.sh
@@ -4,6 +4,7 @@ wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.vimrc -O $HOME
 wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.go.vimrc -O $HOME/.go.vimrc
 wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.coc.vimrc -O $HOME/.coc.vimrc
 wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.airline.vimrc -O $HOME/.airline.vimrc
+wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.go.vimrc -O $HOME/.typescript.vimrc
 wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.tmux.conf -O $HOME/.tmux.conf
 wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.zprofile -O $HOME/.zprofile
 
