6567934a9f74a78aafec8e5d6ce99901a457e484
David Ruiz <53538743+davidlruizc@users.noreply.github.com>
2022-02-23 10:21:58 -0500

    add backup sh and comment .zprofile setup

---
+++ bin/backup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cp $HOME/.go.vimrc .
+cp $HOME/.coc.vimrc .
+cp $HOME/.airline.vimrc .
+cp $HOME/.typescript.vimrc .
+cp $HOME/.tmux.conf .
+
+echo "backup complete :3"
--- bin/nvim-setup.sh
+++ bin/nvim-setup.sh
@@ -6,6 +6,6 @@ wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.coc.vimrc -O $
 wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.airline.vimrc -O $HOME/.airline.vimrc
 wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.go.vimrc -O $HOME/.typescript.vimrc
 wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.tmux.conf -O $HOME/.tmux.conf
-wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.zprofile -O $HOME/.zprofile
+# wget https://raw.githubusercontent.com/davidlruizc/dotfiles/main/.zprofile -O $HOME/.zprofile
 
 echo "neovim setup done <3"
